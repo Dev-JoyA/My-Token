a1953b346083ac586b3ee5c53fde6052bd2b5eea
Joy A <joy.gold13@gmail.com>
2025-08-01 10:27:23 +0100

    fixed deployment error

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");;
 
 const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,5 +15,11 @@ module.exports = {
       accounts: [PRIVATE_KEY],
     }
   },
-};
 
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  }
+
+};
